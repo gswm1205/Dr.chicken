300a1a28a03fb25b26e3ae38d4dae52e521da5cb
raphaelming <spear131121@gmail.com>
2021-12-09 21:17:50 +0900

    first

---
+++ src/main/java/com/smoothie/dr_chicken/DrChickenApplication.java
@@ -0,0 +1,13 @@
+package com.smoothie.dr_chicken;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrChickenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrChickenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smoothie/dr_chicken/DrChickenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smoothie.dr_chicken;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrChickenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
